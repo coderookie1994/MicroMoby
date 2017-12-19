deef9601521bb5bf0d524d10071a7fc0727c4a59
Rupin Jairaj <rupinjairaj@gmail.com>
2017-12-19 20:23:20 +0530

    Add support for CORS

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 	"net/http"
 
 	"github.com/MicroMoby/routers"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	router := routers.InitRoutes()
-	server := &http.Server{
-		Addr:    ":4200",
-		Handler: router,
-	}
+	// server := &http.Server{
+	// 	Addr:    ":4200",
+	// 	Handler: router,
+	// }
 	log.Println("Listening...")
-	err := server.ListenAndServe()
+	err := http.ListenAndServe(":8808", handlers.CORS(handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD"}), handlers.AllowedOrigins([]string{"*"}))(router))
 	if err != nil {
 		panic(err)
 	}
