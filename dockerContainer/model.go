eadb116055534b3ede44bbe83e207dbde5c45791
Rupin Jairaj <rupinjairaj@gmail.com>
2018-01-27 12:16:58 +0530

    Minify the listContainer response message. 	- fix the JSON parsing issues.

--- dockerContainer/model.go
+++ dockerContainer/model.go
@@ -19,4 +19,26 @@ type (
 		ID       string         `json:"id"`
 		Duration *time.Duration `json:"duration"`
 	}
+
+	// ListContainerResponseModel represent the
+	// model that will be sent to the client
+	// on request of a List of containers
+	// for the connected docker daemon.
+	ListContainerResponseModel struct {
+		ID      string   `json:"id"`
+		Names   []string `json:"names"`
+		Image   string   `json:"image"`
+		ImageID string   `json:"imageId"`
+		Command string   `json:"command"`
+		Ports   []port   `json:"ports"`
+		State   string   `json:"state"`
+		Status  string   `json:"status"`
+	}
+
+	port struct {
+		IP          string `json:"ip"`
+		PrivatePort uint16 `json:"privatePort"`
+		PublicPort  uint16 `json:"publicPort"`
+		Type        string `json:"type"`
+	}
 )
